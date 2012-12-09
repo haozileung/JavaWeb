3c5a9f350fd556c80c1c5c9b2b8cb50b08edbf57
Haozi Leung <haozileung.cn@gmail.com>
2012-12-09 18:38:54 +0800

    update old configure

--- src/main/java/com/haozileung/jbloger/common/domain/DomainInterface.java
+++
@@ -1,7 +0,0 @@
-package com.haozileung.jbloger.common.domain;
-
-import java.io.Serializable;
-
-public interface DomainInterface extends Serializable {
-
-}
--- src/main/java/com/haozileung/jbloger/rpst/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.haozileung.jbloger.rpst;
-
-import com.haozileung.jbloger.common.domain.DomainObjectRepository;
-import com.haozileung.jbloger.domain.User;
-import com.haozileung.jbloger.dto.UserInfo;
-
-public interface UserRepository extends DomainObjectRepository<User,UserInfo>{
-	
-}

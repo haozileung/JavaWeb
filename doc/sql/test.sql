adac6cb9c31512f625bdd6e88f5608a141db0662
YamchaL <yamchaleung@gmail.com>
2015-05-03 10:16:35 +0800

    更新sql

--- doc/sql/test.sql
+++ doc/sql/test.sql
@@ -1,16 +1,16 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : localhost
-Source Server Version : 50622
-Source Host           : localhost:3306
+Source Server         : 127.0.0.1
+Source Server Version : 50624
+Source Host           : 127.0.0.1:3306
 Source Database       : test
 
 Target Server Type    : MYSQL
-Target Server Version : 50622
+Target Server Version : 50624
 File Encoding         : 65001
 
-Date: 2015-04-23 16:53:58
+Date: 2015-05-03 10:16:16
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -43,16 +43,23 @@ CREATE TABLE `resource` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `code` varchar(255) NOT NULL,
   `name` varchar(255) NOT NULL,
+  `icon` varchar(255) DEFAULT NULL,
   `url` varchar(255) DEFAULT NULL,
   `type` varchar(255) NOT NULL,
   `group_id` bigint(20) unsigned NOT NULL DEFAULT '0',
   `status` tinyint(4) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of resource
 -- ----------------------------
+INSERT INTO `resource` VALUES ('1', 'system', '系统管理', 'fa fa-gears fa-fw', null, '0', '0', '0');
+INSERT INTO `resource` VALUES ('2', 'index', '首页', 'fa fa-dashboard fa-fw', '/', '1', '0', '0');
+INSERT INTO `resource` VALUES ('3', 'system_user', '用户管理', null, '/user', '1', '1', '0');
+INSERT INTO `resource` VALUES ('4', 'system_role', '角色管理', null, '/role', '1', '1', '0');
+INSERT INTO `resource` VALUES ('5', 'system_resource', '资源管理', null, '/resource', '1', '1', '0');
+INSERT INTO `resource` VALUES ('6', 'system_druid', 'Druid监控', null, '/druid', '1', '1', '0');
 
 -- ----------------------------
 -- Table structure for role
@@ -60,14 +67,17 @@ CREATE TABLE `resource` (
 DROP TABLE IF EXISTS `role`;
 CREATE TABLE `role` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(20) DEFAULT NULL,
+  `code` varchar(20) NOT NULL,
+  `name` varchar(20) NOT NULL,
   `status` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of role
 -- ----------------------------
+INSERT INTO `role` VALUES ('1', 'sa', '超级管理员', '0');
+INSERT INTO `role` VALUES ('3', 'admin', '管理员', '0');
 
 -- ----------------------------
 -- Table structure for role_resource
@@ -80,11 +90,17 @@ CREATE TABLE `role_resource` (
   PRIMARY KEY (`id`),
   KEY `index_resourceId` (`resource_id`) USING BTREE,
   KEY `index_roleId` (`role_id`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of role_resource
 -- ----------------------------
+INSERT INTO `role_resource` VALUES ('1', '1', '1');
+INSERT INTO `role_resource` VALUES ('2', '1', '2');
+INSERT INTO `role_resource` VALUES ('3', '1', '3');
+INSERT INTO `role_resource` VALUES ('4', '1', '4');
+INSERT INTO `role_resource` VALUES ('5', '1', '5');
+INSERT INTO `role_resource` VALUES ('6', '1', '6');
 
 -- ----------------------------
 -- Table structure for user
@@ -102,7 +118,7 @@ CREATE TABLE `user` (
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES ('1', 'haozi', 'haozi@haozileung.com', '2d1ed4af9cec12ea840a89258c76a1b4308db6df91fce21735cc387785927146b462e213492300a25cd563317c90973afec432ab2869763909aa641ddba7362f', '0');
+INSERT INTO `user` VALUES ('1', 'haozi', 'haozi@haozileung.com', 'b14ddfabf73cc5117f1d11328ad764dc094de170937da58833d95ee3f054d3a53876c4a97f6776eb4e04f3d38601ec6b186c74fa7d65bcacf8589be76ca2afdd', '0');
 
 -- ----------------------------
 -- Table structure for user_role
@@ -115,8 +131,9 @@ CREATE TABLE `user_role` (
   PRIMARY KEY (`id`),
   KEY `index_roleId` (`role_id`) USING BTREE,
   KEY `index_userId` (`user_id`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of user_role
 -- ----------------------------
+INSERT INTO `user_role` VALUES ('1', '1', '1');
